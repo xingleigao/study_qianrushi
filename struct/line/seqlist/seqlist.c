650d177c85d2e24556c2da241c1aa22d755df73f
XingleiGao <xingleigao@qq.com>
2021-06-21 11:07:03 +0800

    add file

--- struct/line/seqlist/seqlist.c
+++ struct/line/seqlist/seqlist.c
@@ -25,17 +25,46 @@ void clear_seqlist(seqlist_t *L)
 }
 
 int is_empty_seqlist(seqlist_t *L);
-int is_full_seqlist(seqlist_t *L);
-void set_empty_seqlist(seqlist_t *L);
-int get_length_seqlist(seqlist_t *L);
-void show_seqlist(seqlist_t *L);
+{
 
-int insert_seqlist(seqlist *L,data_t x,int pos)
+}
+int is_full_seqlist(seqlist_t *L)
 {
 	if(L == NULL)
 	{
-		puts("L"
+		puts("seqlist_t *L is NULL");
+		return -1;
 	}
+	return (L->last == MAXSIZE - 1);
+}
+void set_empty_seqlist(seqlist_t *L);
+int get_length_seqlist(seqlist_t *L);
+void show_seqlist(seqlist_t *L)
+{
+	int i = 0;
+	if(L = NULL)
+	{
+		puts("seqlist_t *L is NULL");
+		return;
+	}
+	for(i = 0; i <= L->last;i++)
+		printf("L->data[%d] = %d\n",L,L->data[i]);
+	return;
+}
+
+int insert_seqlist(seqlist *L,data_t x,int pos)
+{
+	int i = 0;
+	if((is_full_seqlist(L))||(pos<0)||(pos>L->last+1))
+	{
+		puts("input argv is invalid");
+		return -1;
+	}
+	for(i = L->last; i >= pos; i--)
+		L->date[i+1] = L->data[i];
+	L->data[pos]=x;
+	L->last++;
+	return 0;
 }
 int delete_seqlist(seqlist *L,int pos);
 int change_seqlist(seqlist *L,data_t x,int pos);
---
+++ struct/line/seqlist/test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "seqlist.h"
+int main(int argv, const char *argv[])
+{
+	
+	return 0;
+}
